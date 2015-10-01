5fd6b766cfe18c6012b1cef9306c1e8e1bb44791
Nacer Khalil <nacerkhalil@gmail.com>
2015-10-01 15:12:12 -0500

    new updates for passport and sequelize

--- Building_Temp_controller/config/passport.js
+++ Building_Temp_controller/config/passport.js
@@ -1,8 +1,8 @@
 // config/passport.js
 
 // load all the things we need
+var passport = require('passport');
 var LocalStrategy   = require('passport-local').Strategy;
-
 // load up the user model
 //var User            = require('../models/user');
 var models  = require('../models');
@@ -23,7 +23,10 @@ module.exports = function(passport) {
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
         models.users.findById(id).then(function(err, user) {
+            console.log("you are here id : "+ user);
             done(err, user);
+        }).error(function(err){
+            done(new Error('User ' + id + ' does not exist'));
         });
     });
 
@@ -43,11 +46,7 @@ module.exports = function(passport) {
 
             // asynchronous
             // User.findOne wont fire unless data is sent back
-            models.users.find({where: {email: email}}).then( function (user, err) {
-                if (err) {
-                    console.log('Error in SignUp: ' + err);
-                    return done(err);
-                }
+            models.users.find({where: {email: email}}).then( function (err,user) {
                 // already exists
                 if (user) {
                     console.log('User already exists');
@@ -69,6 +68,9 @@ module.exports = function(passport) {
                         return done(null, user);
                     });
                 }
+            }).error(function(err){
+                console.log('Error in SignUp: ' + err);
+                return done(err);
             });
         }));
 
@@ -88,13 +90,7 @@ module.exports = function(passport) {
 
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            models.users.find({where: { email :  email}}).then( function(user,err
-            ) {
-                // if there are any errors, return the error before anything else
-
-                if (err)
-                    return done(err);
-
+            models.users.find({where: {email: email}}).then( function(err,user) {
                 // if no user is found, return the message
                 if (!user)
                     return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
@@ -102,9 +98,10 @@ module.exports = function(passport) {
                 // if the user is found but the password is wrong
                 if (!user.methods.validPassword(pass))
                     return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
                 // all is well, return successful user
                 return done(null, user);
+            }).error(function(err){
+                return done(err);
             });
 
         }));
--- Building_Temp_controller/routes/index.js
+++ Building_Temp_controller/routes/index.js
@@ -1,7 +1,11 @@
-
+var models = require('../models');
 module.exports = function(app, passport) {
     /* GET home page. */
     app.get('/', function (req, res, next) {
+        models.User.find({where: {email: 'nacerkhalil@gmail.com'}},(function(err, user) {
+            console.log("you are here id : "+ user);
+
+        }));
         res.render('index', {title: 'Express'});
     });
     app.get('/register', function (req, res, next) {
@@ -18,7 +22,15 @@ module.exports = function(app, passport) {
 
     app.post('/login', passport.authenticate('local-login', {
       successRedirect: '/hola', // redirect to the secure profile section
-      failureRedirect: '/failure', // redirect back to the signup page if there is an error
+      failureRedirect: '/hello', // redirect back to the signup page if there is an error
       failureFlash: true // allow flash messages
     }));
+    app.get('/hola',function(req,res,next) {
+       res.send('you are now logged '+ req.user.firstName);
+    });
+
+    app.use(function(err, req, res, next) {
+        console.error("hey "+err.stack);
+        res.status(500).send('Something broke!');
+    });
 };
