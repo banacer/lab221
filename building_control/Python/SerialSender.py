94006c3b7b5c0e6837345bbac24176a240a626a2
Nacer Khalil <nacerkhalil@gmail.com>
2016-05-05 01:43:09 -0500

    SerialSender.py now follow PEP 8

--- building_control/Python/SerialSender.py
+++ building_control/Python/SerialSender.py
@@ -1,36 +1,43 @@
+'''
+A Serial communication module with Arduino to read
+temperature, humidity, CO2 and control the damper
+'''
 from time import sleep
 import serial
 
-class SerialSender:
-    def __init__(self,serial_):
-        self.ser = serial.Serial(serial_,9600,timeout=1)
+class SerialSender(object):
+    '''
+    Class to get temperature, humidity, CO2 and control
+    the damper connected to an arduino
+    '''
+    def __init__(self, serial_):
+        self.ser = serial.Serial(serial_, 9600, timeout=1)
 
-    def sendDamperCmd(self, val):
+    def send_damper_command(self, val):
         """
         send command to damper to control
         :param val: the val of the damper between 0 and 100
         """
-        self.ser.write('d');
+        self.ser.write('d')
         self.ser.write(chr(val))
         ack = self.ser.readline().rstrip()
         if ack == 'A':
-            return
+            pass
         else:
             raise Exception('Damper command failed')
 
-    def getTemp(self):
+    def get_temp(self):
         """
         get temperature
         :return: temperature in F
         """
-        ret = self.ser.write('t')
+        self.ser.write('t')
         self.ser.flushOutput()
         val = self.ser.readline()
-        print 'val is ',val
         val = val.rstrip()
         return float(val)
 
-    def getHumidity(self):
+    def get_humidity(self):
         """
         get humidity
         :return: humidity in %
@@ -39,7 +46,7 @@ class SerialSender:
         val = self.ser.readline().rstrip()
         return float(val)
 
-    def getCO2(self):
+    def get_co2(self):
         """
         get CO2
         :return: CO2 in p
@@ -53,14 +60,15 @@ class SerialSender:
         set room temp to requested temp in a rather simplistic fashion
         :param target_temp: the temperature to be set
         """
-        current_temp = self.getTemp()
+        current_temp = self.get_temp()
         damper_opened = False
-        if target_temp < current_temp:  # if target is smaller then we can cool the room if not we expect the someone to manually turn on the heater
-            self.sendDamperCmd(100)
+        if target_temp < current_temp:  # if target is smaller then we can cool the
+        # room if not we expect the someone to manually turn on the heater
+            self.send_damper_command(100)
             damper_opened = True
 
         while target_temp != current_temp:
             sleep(10)
-            current_temp = self.getTemp()
-        if damper_opened == True:
-            self.sendDamperCmd(0)
+            current_temp = self.get_temp()
+        if damper_opened is True:
+            self.send_damper_command(0)
